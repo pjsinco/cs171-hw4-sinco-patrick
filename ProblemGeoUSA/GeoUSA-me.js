ea68df7afc0948be7ea73bafee9b32b5ffa0626e
Patrick Sinco <psinco@gmail.com>
2014-03-17 12:14:00 -0500

    get map to appear; zoom function is zooming but not centering correctly

--- ProblemGeoUSA/GeoUSA-me.js
+++ ProblemGeoUSA/GeoUSA-me.js
@@ -8,6 +8,7 @@ var margin = {
 
 var width = 1060 - margin.left - margin.right; // 960
 var height = 800 - margin.top - margin.bottom; // 700
+var centered;
 
 var bbVis = {
   x: 100,
@@ -16,14 +17,133 @@ var bbVis = {
   h: 300
 }
 
-var detailVis = d3.select('#detailVis')
-  .append('svg')
+var detailVis = d3.select('#detailVis').append('svg')
   .attr('width', 350)
   .attr('height', 200)
 
-var canvas = d3.select('#vis')
-  .append('svg')
+var svg = d3.select('#vis').append('svg')
   .attr('width', width + margin.left + margin.right)
   .attr('height', height + margin.top + margin.bottom)
-  
 
+//svg
+//  .append('rect')
+//  .attr('class', 'background')
+//  .attr('width', width)
+//  .attr('height', height)
+//  .style('fill', 'none')
+//  .on('clicked', click);
+
+var g = svg.append('g')
+  .attr('class', 'country')
+  .attr('transform', 'translate(' + margin.left + ','
+    + margin.right + ')')
+  
+var projection = d3.geo.albersUsa()
+  .translate([width / 2, height / 2])
+  .precision(0.1)
+
+var path = d3.geo.path()
+  .projection(projection)
+
+var dataset = [];
+var completeDataSet = [];
+
+d3.json('../data/us-named.json', function(error, data) {
+  //console.log(data);
+
+  // convert topojson to geojson
+  var usMap = topojson.feature(data, data.objects.states);
+  //console.log(usMap);
+
+  g
+    .selectAll('.state')
+    .data(usMap.features)
+    .enter()
+      .append('path')
+      .attr('class', 'state')
+      .attr('d', path)
+      .on('click', clicked)
+
+  // needed? 
+  g
+    .append('path')
+    .datum(topojson.mesh(data, data.objects.states, function(a, b) {
+      return a !== b;
+    }));
+  
+  //loadStats();
+
+}); // end d3.json -- us-named
+
+function clicked(d) {
+  console.log(d);
+  console.log(d.properties.name);
+  console.log(d3.geo.centroid(d));
+  var x, y, k;
+  
+  if (d && centered !== d) {
+    var centroid = d3.geo.centroid(d);
+    x = centroid[0];
+    y = centroid[1];
+    k = 4;
+    centered = d;
+  } else {
+    x = width / 2;
+    y = height / 2;
+    k = 1;
+    centered = null;
+  }
+
+  g
+    .selectAll('path')
+    .classed('active', centered && function(d) {
+      return d === centered;
+    })
+
+  g
+    .transition()
+    .duration(750)
+    .attr('transform', 'translate(' + width / 2 + ','
+      + height / 2 + ')scale(' + k + ')translate('
+      + -x + ',' + -y + ')');
+
+//  svg
+//    .select('.country')
+//    .append('circle')
+//    .attr('cx', function (d) {
+//      return path(d3.geo.centroid(d)[0]);
+//    })
+//    .attr('cy', function (d) {
+//      return path(d3.geo.centroid(d)[1]);
+//    })
+//    .style('fill', 'gold')
+}
+
+function loadStations() {
+  d3.csv('../data/NSRCB_StationsMeta.csv', function(error, data) {
+
+    // ..
+
+  }); // end d3.csv() -- stationsmeta
+} // end loadStations()
+    
+function loadStats() {
+  d3.json('../data/reduceMonthStationHour2003_2004.json',
+    function(error, data) {
+
+    completeDataset = data;
+    
+    // ..
+    
+    loadStations();
+
+  }); // end d3.json()
+} // end loadStats()
+  
+function zoomIn(d) {
+  console.log(d.properties.name);
+}
+
+function zoomOut() {
+
+}
