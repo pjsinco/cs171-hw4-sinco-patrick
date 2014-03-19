3ce6c0abb025481382c0e84c1eccdde4d913fbb8
pjsinco <psinco@fas.harvard.edu>
2014-03-19 17:38:52 -0500

    wrangles data into correct format

--- ProblemGeoUSA/aggregation.js
+++ ProblemGeoUSA/aggregation.js
@@ -1,47 +1,36 @@
 d3.json('../data/allData2003_2004.json', function(error, data) {
-//d3.json('../data/allData-sample-2.json', function(error, data) {
-  var timeParser = d3.time.format("%b %-d, %Y %X %p");
-
+  //var timeParser = d3.time.format("%b %-d, %Y %X %p");
   //var timeParser = d3.time.format("%b %-d, %Y %H:%-M:%S %p");
 
-  var stations = {}
-  
+  var stations = {};
+
   for (var s in data) {
+
     var station = {
         sum: 0,
-        hourly: {
-          '01:00:00': 0,
-          '02:00:00': 0,
-          '03:00:00': 0,
-          '04:00:00': 0,
-          '05:00:00': 0,
-          '06:00:00': 0,
-          '07:00:00': 0,
-          '08:00:00': 0,
-          '09:00:00': 0,
-          '10:00:00': 0,
-          '11:00:00': 0,
-          '12:00:00': 0,
-          '13:00:00': 0,
-          '14:00:00': 0,
-          '15:00:00': 0,
-          '16:00:00': 0,
-          '17:00:00': 0,
-          '18:00:00': 0,
-          '19:00:00': 0,
-          '20:00:00': 0,
-          '21:00:00': 0,
-          '22:00:00': 0,
-          '23:00:00': 0,
-          '00:00:00': 0
-        }
-    } 
+        hourly: []
+    };
+
+    //initialize hourly with 0's
+    var hourly = [];
+    for (var i = 0; i < 24; i++) { 
+      hourly[i] = 0; 
+    };
+
     var sum = 0;
-    data[s].forEach(function(d) {
-      sum += parseFloat(d['value']);
-    })
     station['sum'] = sum;
+
+    // for every date-value pair in the station...
+    data[s].forEach(function(d, i) {
+      var val = parseFloat(d['value']);
+      hourly[i % 24] += val;
+      station['sum'] += val;
+      station['hourly'] = hourly;
+    });
+
+    // add the new station to the stations objects
     stations[s] = station;
-  }
-  console.log(stations);
+
+  }; // end for()
+
 }); // end d3.json() -- alldata
